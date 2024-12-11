86665dcec47e0b213d7702f14837eb96ebad3c24
shoyonp <shoyonkumar143@gmail.com>
2024-12-12 04:18:55 +0600

    job information got

--- index.js
+++ index.js
@@ -57,6 +57,19 @@ async function run() {
       const email = req.query.email;
       const query = { applicant_email: email };
       const result = await jobApplicationCollectiion.find(query).toArray();
+
+      //   bad way to aggergate data
+      for (const application of result) {
+        console.log(application.job_id);
+        const query1 = { _id: new ObjectId(application.job_id) };
+        const job = await jobsCollection.findOne(query1);
+        if (job) {
+          application.title = job.title;
+          application.company = job.company;
+          application.company_logo = job.company_logo;
+        }
+      }
+
       res.send(result);
     });
 
