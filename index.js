035831fa36fb3b07ebfed1387386ebc35a8b29ea
shoyonp <shoyonkumar143@gmail.com>
2024-12-13 03:32:12 +0600

    status updated

--- index.js
+++ index.js
@@ -124,6 +124,22 @@ async function run() {
 
       res.send(result);
     });
+
+    app.patch("/job-applications/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: data.status,
+        },
+      };
+      const result = await jobApplicationCollectiion.updateOne(
+        filter,
+        updatedDoc
+      );
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
